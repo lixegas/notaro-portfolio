7cd2426d592ccfdb6914465168718b5db2a5edff
Lixegas <167356473+lixegas@users.noreply.github.com>
2025-05-02 17:37:12 +0200

    Small changes

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+// next.config.mjs
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  distDir: 'dist',
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
