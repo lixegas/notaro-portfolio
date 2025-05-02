d5c5e2d3c786d4c6743c8a9a1d595c9368026f76
Lixegas <167356473+lixegas@users.noreply.github.com>
2025-05-02 19:25:03 +0200

    fixed2

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
-// next.config.mjs
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  distDir: 'dist',
-  images: {
-    unoptimized: true,
-  },
+
 };
 
 export default nextConfig;
